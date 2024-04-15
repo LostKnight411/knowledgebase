549482610ee9804c8914ea5006713339bd6028cb
kevin8181 <66894759+kevin8181@users>
2024-04-15 12:13:04 -0400

    tina on vercel

---
+++ api/tina/backend.ts
@@ -0,0 +1,49 @@
+// https://tina.io/docs/reference/self-hosted/tina-backend/vercel-functions/
+
+import {
+	TinaNodeBackend,
+	LocalBackendAuthProvider,
+	createDatabase,
+	createLocalDatabase,
+} from "@tinacms/datalayer";
+import { TinaAuthJSOptions, AuthJsBackendAuthProvider } from "tinacms-authjs";
+import { RedisLevel } from "upstash-redis-level";
+
+const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === "true";
+const branch =
+	process.env.GITHUB_BRANCH ||
+	process.env.VERCEL_GIT_COMMIT_REF ||
+	process.env.HEAD ||
+	"main";
+
+const databaseClient = isLocal
+	? createLocalDatabase()
+	: createDatabase({
+			databaseAdapter: new RedisLevel({
+				namespace: branch,
+				redis: {
+					url: process.env.KV_REST_API_URL || "http://localhost:8079",
+					token: process.env.KV_REST_API_TOKEN || "example_token",
+				},
+				debug: process.env.DEBUG === "true" || false,
+			}),
+		});
+
+const authProvider = isLocal
+	? LocalBackendAuthProvider()
+	: AuthJsBackendAuthProvider({
+			authOptions: TinaAuthJSOptions({
+				databaseClient: databaseClient,
+				secret: process.env.NEXTAUTH_SECRET!,
+			}),
+		});
+
+const tinaHandler = TinaNodeBackend({
+	authProvider,
+	databaseClient,
+});
+
+export default (req: any, res: any) => {
+	// Modify the request here if you need to
+	return tinaHandler(req, res);
+};
