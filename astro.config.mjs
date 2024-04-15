5d35d2445434b5999c11afe273f1e0d62c7a1c4c
kevin8181 <66894759+kevin8181@users>
2024-04-14 22:15:53 -0400

    basic setup and theming

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,55 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 import tailwind from "@astrojs/tailwind";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: 'My Docs',
-    social: {
-      github: 'https://github.com/withastro/starlight'
-    },
-    sidebar: [{
-      label: 'Guides',
-      items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: 'Example Guide',
-        link: '/guides/example/'
-      }]
-    }, {
-      label: 'Reference',
-      autogenerate: {
-        directory: 'reference'
-      }
-    }]
-  }), tailwind()]
+	integrations: [
+		starlight({
+			title: "The Great Outdoors",
+			logo: {
+				src: "./src/assets/logo.svg",
+			},
+			customCss: ["./src/tailwind.css"],
+			editLink: {
+				baseUrl: "https://github.com/tgoHQ/starlight/edit/main/",
+			},
+			social: {
+				github: "https://github.com/tgoHQ",
+				discord: "https://discord.gg/the-great-outdoors-345621611770282004",
+			},
+			sidebar: [
+				{
+					label: "Guides",
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{
+							label: "Example Guide",
+							link: "/guides/example/",
+						},
+					],
+				},
+				{
+					label: "Reference",
+					autogenerate: {
+						directory: "reference",
+					},
+				},
+				{
+					label: "External Resources",
+					collapsed: true,
+					items: [
+						{
+							label: "Lighterpack",
+							link: "https://lighterpack.com",
+						},
+					],
+				},
+			],
+		}),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
+	site: "https://starlight-lovat.vercel.app/",
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,33 @@
+import starlightPlugin from "@astrojs/starlight-tailwind";
+
+// Generated color palettes https://starlight.astro.build/guides/css-and-tailwind/
+const accent = {
+	
+	200: "#298D61",
+	600: "#137c5a",
+	900: "#f00",
+	950: "#033E2C",
+};
+const gray = {
+	100: "#f5f6f8",
+	200: "#eceef2",
+	300: "#c0c2c7",
+	400: "#888b96",
+	500: "#545861",
+	700: "#353841",
+	800: "#24272f",
+	900: "#17181c",
+};
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: { accent, gray },
+		},
 	},
-	plugins: [],
-}
+	plugins: [starlightPlugin()],
+};
+
+import starlightPlugin from "@astrojs/starlight-tailwind";
