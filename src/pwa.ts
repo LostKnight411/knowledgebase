da927c0a44a045ae9ff87461c7f1efd5cd3b0230
kevin8181 <66894759+kevin8181@users>
2024-04-19 22:10:54 -0400

    additional sw logging

--- src/pwa.ts
+++ src/pwa.ts
@@ -2,8 +2,14 @@ import { registerSW } from 'virtual:pwa-register';
 
 registerSW({
 	immediate: true,
-	onRegisteredSW(swScriptUrl) {
-		console.log('SW registered: ', swScriptUrl);
+	onRegisteredSW(swScriptUrl, registration) {
+		console.log('SW registered: ', swScriptUrl, registration);
+	},
+	onRegisterError(error) {
+		console.log('SW registration error: ', error);
+	},
+	onNeedRefresh() {
+		console.log('SW need refresh');
 	},
 	onOfflineReady() {
 		console.log('PWA application ready to work offline');
